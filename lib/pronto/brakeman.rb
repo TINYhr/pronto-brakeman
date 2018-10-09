b860b21bef223fd165a5c7a89328766c47047857
huydinh <dinhhuydh@gmail.com>
2018-10-09 14:00:30 +0700

    Ignore haml views

--- lib/pronto/brakeman.rb
+++ lib/pronto/brakeman.rb
@@ -11,7 +11,7 @@ module Pronto
       return [] unless files.any?
 
       output = ::Brakeman.run(app_path: repo_path,
-                              output_formats: [:to_s], skip_files: 'app/views/')
+                              output_formats: [:to_s], skip_files: Dir["app/views/**/*.haml"])
       messages_for(ruby_patches, output).compact
     rescue ::Brakeman::NoApplication
       []
